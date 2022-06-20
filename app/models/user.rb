efcf1d9f25ec8a38dd6095745c30b1e6eee82300
Fabrizio <fabgrel10@gmail.com>
2022-06-19 21:57:41 -0500

    🔒️ Add validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :email, uniqueness: true
+  validates_format_of :email, with: VALID_EMAIL_REGEX
+  validates :password_digest, presence: true
 end
