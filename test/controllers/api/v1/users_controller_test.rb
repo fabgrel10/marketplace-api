6cb9accf45b53d8e8037f0345e0742a4f92d90af
Fabrizio <fabgrel10@gmail.com>
2022-06-26 15:49:26 -0500

    🔒️ Restrict actions for unauthorized users

--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -29,13 +29,16 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should update user' do
-    patch api_v1_user_url(@user), params: { user: { email: @user.email, password: '123456' } }, as: :json
+    patch api_v1_user_url(@user),
+          params: { user: { email: @user.email } },
+          headers: { Authorization: JsonWebToken.encode(user_id: @user.id) },
+          as: :json
     assert_response :success
   end
 
-  test 'should not update user when invalid params are sent' do
-    patch api_v1_user_url(@user), params: { user: { email: 'bad_email', password: '123456' } }, as: :json
-    assert_response :unprocessable_entity
+  test 'should forbid update user' do
+    patch api_v1_user_url(@user), params: { user: { email: @user.email } }, as: :json
+    assert_response :forbidden
   end
 
   test 'should destroy user' do
@@ -43,5 +46,12 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
       delete api_v1_user_url(@user), headers: { Authorization: JsonWebToken.encode(user_id: @user.id) }, as: :json
     end
     assert_response :no_content
+  end
+
+  test 'should forbid destroy user' do
+    assert_no_difference('User.count') do
+      delete api_v1_user_url(@user), as: :json
+    end
+    assert_response :forbidden
   end
 end
